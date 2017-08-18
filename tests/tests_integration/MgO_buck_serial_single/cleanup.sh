dcf6abd2cfb6dd7a0d22bf63a9a716d2a4931467
Eugene J. Ragasa <eragasa@ufl.edu>
2017-08-18 04:01:57 -0400

    added workflow and task abstract classes

---
+++ tests/tests_integration/MgO_buck_serial_single/cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm results.out
+rm -rf MgO_NaCl.min
+rm -rf MgO_NaCl.elas
+rm -rf MgO_NaCl.sp
+rm -rf MgO_NaCl*
---
+++ tests/tests_integration/MgO_buck_serial_single/run_my_minerva.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LAMMPS_BIN=/usr/local/bin/lammps
+export EVENT_NOKQUEUE=1
+python buckingham_single.py
