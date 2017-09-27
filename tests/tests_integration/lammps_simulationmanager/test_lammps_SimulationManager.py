a3555dd7cba4455f04fbb3e640706364ad843b9e
Eugene J. Ragasa <eragasa@ufl.edu>
2017-09-26 20:45:28 -0400

    more organization of testing scripts for LAMMPS.

--- tests/tests_integration/lammps_simulationmanager/test_lammps_SimulationManager.py
+++ tests/tests_integration/lammps_simulationmanager/test_lammps_SimulationManager.py
@@ -19,6 +19,8 @@ def get_lewis_catlow_parameters():
     return copy.deepcopy(param_dict)
 
 if __name__ == "__main__":
+    is_debug = True
+
     qoi_config_file = "pypospack.qoi.yaml"
     structure_config_file = "pypospack.structure.yaml"
     potential_config_file = "pypospack.buckingham.yaml"
@@ -34,8 +36,25 @@ if __name__ == "__main__":
 
     qoi_manager = qoi.QoiManager(qoi_info)
     required_simulations = qoi_manager.get_required_simulations()
-    for k,v in qoi_manager.required_simulations.items():
-        print(k,'\n\t',v)
-    sim_manager = lammps.SimulationManager()
 
-    
+    if is_debug:
+        for k,v in qoi_manager.required_simulations.items():
+            print(k)
+            for k2, v2 in v.items():
+                print('\t',k2,':',v2)
+
+    sim_manager = lammps.SimulationManager()
+    sim_manager.add_required_simulations(
+            required_simulations = qoi_manager.get_required_simulations())
+    sim_manager.structure_info = structure_info
+    sim_manager.potential_info = potential_info
+    if is_debug:
+        print(80*"-")
+        print('lammps.SimulationManager.lmps_sim_obj')
+        print(80*"-")
+        for k,v in sim_manager.obj_lammps_tasks.items():
+            print(k,":",v)
+
+    sim_manager.evaluate_parameters(
+            param_dict = get_lewis_catlow_parameters())
+
