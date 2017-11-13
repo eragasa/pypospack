34e280ee26eaa942893b7405ed536ea831d8d858
Eugene J. Ragasa <eragasa@ufl.edu>
2017-11-13 14:04:25 -0500

    GulpPhononCalculation debugged and added test coverage

--- pypospack/task/tasks_gulp/gulp_phonons.py
+++ pypospack/task/tasks_gulp/gulp_phonons.py
@@ -13,33 +13,14 @@ class GulpPhononCalculation(GulpSimulation):
             structure_filename,
             restart=False
         ):
+        self.shrink=[8,8,8]
+        self.kpoints=[10,10,10]
         # initialize the parent class
         GulpSimulation.__init__(self,
                 task_name=task_name,
                 task_directory=task_directory,
                 structure_filename=structure_filename,
                 )
-
-        self.shrink=[8,8,8]
-        self.kpoints=[10,10,10]
-
-    def setup_task_directory(self):
-        self.gulp_input_filename = os.path.join(
-                self.task_directory,'gulp.in')
-        self.write_gulp_input_file(
-                filename=self.gulp_input_filename,
-                poscar=self.structure_filename)
-
-    def run(self):
-        cmd_str = '{} < gulp.in > gulp.out 2>/dev/null'.format(self.gulp_bin)
-
-        original_directory = os.getcwd()
-        os.chdir(self.task_directory)
-
-        try:
-            subprocess(cmd_str,shell=True)
-        finally:
-            os.chdir(original_directory)
 
     def write_gulp_input_file(self,filename=None,structure_filename=None):
         """
@@ -54,31 +35,17 @@ class GulpPhononCalculation(GulpSimulation):
             self.structure_filename=structure_filename
 
         str_out = "opti conp prop phon eigen\n"
-        str_out += self.get_structure_section(filename)
-        str_out += self.get_potential_section(parameters)
-        str_out += self.get_phonon_section()
+        str_out += self.get_gulpinputfile_structuresection()
+        str_out += self.get_gulpinputfile_potentialsection()
+        str_out += self.get_gulpinputfile_phononsection()
 
-        with open(self.gulp_input_filename,'w') as f:
+        gulp_input_filename = os.path.join(
+                self.task_directory,
+                self.gulp_input_filename)
+        with open(gulp_input_filename,'w') as f:
             f.write(str_out)
 
-    def get_structure_section(self,filename=None):
-
-        if filename is not None:
-            self.structure_filename = filename
-
-        # read in the poscar file
-        self.structure = vasp.Poscar()
-        self.structure.read(self.structure_filename)
-
-        H = sim_cell.H * sim_cell.a0
-        str_out = "vectors\n"
-        str_out += "{:.10f} {:.10f} {:.10f}\n".format(H[0,0],H[0,1],H[0,2])
-        str_out += "{:.10f} {:.10f} {:.10f}\n".format(H[1,0],H[1,1],H[1,2])
-        str_out += "{:.10f} {:.10f} {:.10f}\n".format(H[2,0],H[2,1],H[2,2])
-        str_out += "fractional\n"
-    
-
-    def get_phonon_section(self):
+    def get_gulpinputfile_phononsection(self):
 
         str_phonon = (
                 "shrink\n"
@@ -91,8 +58,8 @@ class GulpPhononCalculation(GulpSimulation):
                 ).format(
                         shrink1=self.shrink[0],
                         shrink2=self.shrink[1],
-                        shirnk3=self.shrink[2],
+                        shrink3=self.shrink[2],
                         k1=self.kpoints[0],
                         k2=self.kpoints[1],
-                        k3=self.points[2])
-        return str_out
+                        k3=self.kpoints[2])
+        return str_phonon
