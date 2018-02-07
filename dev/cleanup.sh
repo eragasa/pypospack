8ac8beace6d1444a0943d58f7e928da164d4aa16
Manny Esparragoza <manuelesp@ufl.edu>
2018-02-07 15:57:02 -0500

    Initial files

---
+++ dev/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm pypospack.config.in
+rm -rf MgO_NaCl.lmps_min_all  
+rm -rf MgO_NaCl.lmps_elastic
+rm -rf MgO_NaCl_fr_a.lmps_min_pos
+rm -rf Si.lmps_elastic
+rm -rf Si.lmps_min_all
+
