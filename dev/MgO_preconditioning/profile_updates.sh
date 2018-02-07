97dedde7f63508eb8d2cc74cce622337184dbc50
Eugene J. Ragasa <eragasa@ufl.edu>
2018-02-07 16:09:39 -0500

    profile_updates.sh for manny

---
+++ dev/MgO_preconditioning/profile_updates.sh
@@ -0,0 +1,7 @@
+
+# pythonpath
+export PYTHONPATH=$(cd ~/repos/pypospack;pwd):$PYTHONPATH
+
+export EVENT_NOKQUEUE=1  # fixes kq_init, tmux error
+export LAMMPS_BIN=/usr/local/bin/lammps
+export LAMMPS_SERIAL_BIN=/usr/local/bin/lammps
