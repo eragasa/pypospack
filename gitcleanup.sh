1d523af797cb39650de6364e0f35ffd79a5de7d1
Eugene J. Ragasa <eragasa@ufl.edu>
2018-02-12 01:14:51 -0500

    fixed bug in the gitcleanup

--- gitcleanup.sh
+++ gitcleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
-find . | grep -E "(\.lmps_elastic|\.lmps_min_all)" | xargs rm -rf
+find . | grep -E "(\.lmps_elastic|\.lmps_min_all)"
