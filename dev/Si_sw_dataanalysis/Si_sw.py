5d46265d84a14968fdc5a2963d5cc0c3234ee100
Eugene J. Ragasa <eragasa@ufl.edu>
2018-02-28 16:36:38 -0500

    Si data analysis

--- dev/Si_sw_dataanalysis/Si_sw.py
+++ dev/Si_sw_dataanalysis/Si_sw.py
@@ -4,10 +4,10 @@ from pypospack.qoi import QoiDatabase
 #-----------------------------------------------------------------------------
 # DEFINE STRUCTURES
 #-----------------------------------------------------------------------------
-Si_sw_structures = OrderedDict()
-Si_sw_structures['structure_directory'] = 'Si_structures'
-Si_sw_structures['structures'] = OrderedDict()
-Si_sw_structures['structures']['Si_dia'] = 'Si_dia_unit.vasp'
+structure_db = OrderedDict()
+structure_db['structure_directory'] = 'Si_structures'
+structure_db['structures'] = OrderedDict()
+structure_db['structures']['Si_dia'] = 'Si_dia_unit.vasp'
 
 #  in this case 'Si_dia' is the short name which the rest of the file
 #  refers to the structure file which is contained in
@@ -15,77 +15,69 @@ Si_sw_structures['structures']['Si_dia'] = 'Si_dia_unit.vasp'
 #-----------------------------------------------------------------------------
 # DEFINE POTENTIAL FORMALISM
 #-----------------------------------------------------------------------------
-potential = OrderedDict()
-potential['potential_type'] = 'stillingerweber'
-potential['symbols'] = ['Si']
-potential['cutoff_global'] = 10.0
+potential_formalism = OrderedDict()
+potential_formalism['potential_type'] = 'stillingerweber'
+potential_formalism['symbols'] = ['Si']
+#potential_formalism['cutoff_global'] = 10.0
 
-Si_sw_qoi_db = QoiDatabase()
-Si_sw_qoi_db.add_qoi(
+#-----------------------------------------------------------------------------
+# QOI DEFINITIONS
+#-----------------------------------------------------------------------------
+qoi_db = QoiDatabase()
+qoi_db.add_qoi(
         qoi_name='Si_dia.a0',
         qoi_type='a11_min_all',
         structures=OrderedDict([('ideal','Si_dia')]),
         target=5.431)
-Si_sw_qoi_db.add_qoi(
+qoi_db.add_qoi(
         qoi_name='Si_dia.c12',
         qoi_type='c12',
         structures=OrderedDict([('ideal','Si_dia')]),
         target=75.00)
-Si_sw_qoi_db.add_qoi(
+qoi_db.add_qoi(
         qoi_name='Si_dia.c44',
         qoi_type='c44',
         structures=OrderedDict([('ideal','Si_dia')]),
         target=56.00)
-Si_sw_qoi_db.add_qoi(
+qoi_db.add_qoi(
         qoi_name='Si_dia.B',
         qoi_type='bulk_modulus',
         structures=OrderedDict([('ideal','Si_dia')]),
         target=100.00)
+#<----------------- qoi performance constraints
+qoi_constraints = OrderedDict()
+for qoi_name, qoi_info in qoi_db.qois.items():
+    qoi_constraints[qoi_name] = qoi_info['target'] * 0.20
 # <---------------- SAMPLING CONFIGURATION
-Si_sw_sampling = OrderedDict()
-Si_sw_sampling['n_iterations'] = 10
-Si_sw_sampling['mc_seed'] = None
+sampling = OrderedDict()
+sampling['n_iterations'] = 10
+sampling['mc_seed'] = None
 # <---------------- INITIAL DEFAULT CONFIGURATION
-for i in range(Si_sw_sampling['n_iterations']):
-  Si_sw_sampling[i] = OrderedDict()
-  Si_sw_sampling[i]['type'] = 'kde'
-  Si_sw_sampling[i]['n_samples'] = 10000
+for i in range(sampling['n_iterations']):
+  sampling[i] = OrderedDict()
+  sampling[i]['type'] = 'kde'
+  sampling[i]['n_samples'] = 10000
 # <---------------- OVERRIDE DEFAULT CONFIGURATION, FOR I=0
-Si_sw_sampling[0]['type'] = 'parametric'
+sampling[0]['type'] = 'parametric'
 #<----------------- determine parameters
 
 #<----------------- free parameters
 # For uniform distributions, 
 #     a = is the low of the rnage, 
 #     b = is the high of the
-Si_sw_param_dist = OrderedDict()
-Si_sw_param_dist['SiSiSi_epsilon'] = ['uniform',{'a': 2.1, 'b':2.2}]
-Si_sw_param_dist['SiSiSi_sigma'] = ['uniform',{'a': 1.0, 'b':3.0}] 
-Si_sw_param_dist['SiSiSi_a'] = ['uniform',{'a': 1.5, 'b':2.0}]
-Si_sw_param_dist['SiSiSi_lambda'] = ['uniform',{'a': 20.0, 'b':32}]
-Si_sw_param_dist['SiSiSi_gamma'] = ['uniform',{'a': 1.0, 'b':2.0}]
-Si_sw_param_dist['SiSiSi_costheta0'] = ['equals',-1/3.]
-Si_sw_param_dist['SiSiSi_A'] = ['uniform',{'a': 6.0, 'b':20.0}]
-Si_sw_param_dist['SiSiSi_B'] = ['uniform',{'a': 0.5, 'b':1.0}]
-Si_sw_param_dist['SiSiSi_p'] = ['equals',4.0]
-Si_sw_param_dist['SiSiSi_q'] = ['equals',0.0]
-Si_sw_param_dist['SiSiSi_tol'] = ['equals',0.0]
-
-
-#<----------------- qoi performance constraints
-Si_sw_qoi_constraints = OrderedDict()
-
-# define performance constraints as 20 of the qoi target value
-for qoi_name, qoi_info in Si_sw_qoi_db.qois.items():
-    Si_sw_qoi_constraints[qoi_name] = qoi_info['target'] * 0.20
-
-# print out qoi performance constraints
-print(80*'-')
-print('{:^80}'.format('QOI PERFORMANCE CONSTRAINTS'))
-print(80*'-')
-
-for qoi_name, value in Si_sw_qoi_constraints.items():
-    print('{:>20} {:>10}'.format(qoi_name,value))
-
+parameter_distribution = OrderedDict()
+parameter_distribution['SiSiSi_epsilon'] = ['uniform',{'a': 2.1, 'b':2.2}]
+parameter_distribution['SiSiSi_sigma'] = ['uniform',{'a': 1.0, 'b':3.0}] 
+parameter_distribution['SiSiSi_a'] = ['uniform',{'a': 1.5, 'b':2.0}]
+parameter_distribution['SiSiSi_lambda'] = ['uniform',{'a': 20.0, 'b':32}]
+parameter_distribution['SiSiSi_gamma'] = ['uniform',{'a': 1.0, 'b':2.0}]
+parameter_distribution['SiSiSi_costheta0'] = ['equals',-1/3.]
+parameter_distribution['SiSiSi_A'] = ['uniform',{'a': 6.0, 'b':20.0}]
+parameter_distribution['SiSiSi_B'] = ['uniform',{'a': 0.5, 'b':1.0}]
+parameter_distribution['SiSiSi_p'] = ['equals',4.0]
+parameter_distribution['SiSiSi_q'] = ['equals',0.0]
+parameter_distribution['SiSiSi_tol'] = ['equals',0.0]
+#<----------------- parameter constraints
+parameter_constraints = OrderedDict()
 
 
