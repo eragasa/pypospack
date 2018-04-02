d302ae934da21d00d50343364a1d1bf9b9c52991
Manuel Esparragoza <manuelesp@ufl.edu>
2018-04-02 16:55:29 -0400

    manny stuff

--- dev/MgO_iterative_conditioner/MgO_condition_by_pressure/data/MgO__buck_coul.py
+++ dev/MgO_iterative_conditioner/MgO_condition_by_pressure/data/MgO__buck_coul.py
@@ -6,13 +6,13 @@ from pypospack.qoi import QoiDatabase
 #------------------------------------------------------------------------------
 # <---------------- SAMPLING CONFIGURATION
 sampling = OrderedDict()
-sampling['n_iterations'] = 2
+sampling['n_iterations'] = 1
 sampling['mc_seed'] = None
 # <---------------- INITIAL DEFAULT CONFIGURATION
 for i in range(sampling['n_iterations']):
     sampling[i] = OrderedDict()
     sampling[i]['type'] = 'kde'
-    sampling[i]['n_samples'] = 500
+    sampling[i]['n_samples'] = 50000
 # <---------------- OVERRIDE DEFAULT CONFIGURATION, FOR I=0
 sampling[0]['type'] = 'parametric'
 # sampling[0]['type'] = 'from_file'
@@ -67,63 +67,10 @@ structure_db['structures']['MgO_NaCl_001s'] = 'MgO_NaCl_001s.vasp'
 #------------------------------------------------------------------------------
 qoi_db = QoiDatabase()
 qoi_db.add_qoi(
-        qoi_name='MgO_NaCl.a0',
-        qoi_type='a11_min_all',
+        qoi_name='MgO_NaCl.p11',
+        qoi_type='p_11_min_none',
         structures=OrderedDict([('ideal','MgO_NaCl')]),
         target=4.246)
-qoi_db.add_qoi(
-        qoi_name='MgO_NaCl.c11',
-        qoi_type='c11',
-        structures=OrderedDict([('ideal','MgO_NaCl')]),
-        target=277.00)
-qoi_db.add_qoi(
-        qoi_name='MgO_NaCl.c12',
-        qoi_type='c12',
-        structures=OrderedDict([('ideal','MgO_NaCl')]),
-        target=91.67)
-qoi_db.add_qoi(
-        qoi_name='MgO_NaCl.c44',
-        qoi_type='c44',
-        structures=OrderedDict([('ideal','MgO_NaCl')]),
-        target=144.01)
-qoi_db.add_qoi(
-        qoi_name='MgO_NaCl.B',
-        qoi_type='bulk_modulus',
-        structures=OrderedDict([('ideal','MgO_NaCl')]),
-        target=153.45)
-qoi_db.add_qoi(
-        qoi_name='MgO_NaCl.G',
-        qoi_type='shear_modulus',
-        structures=OrderedDict([('ideal','MgO_NaCl')]),
-        target=92.66)
-qoi_db.add_qoi(
-        qoi_name='MgO_NaCl.fr_a',
-        qoi_type='E_formation',
-        structures=OrderedDict([
-            ('defect','MgO_NaCl_fr_a'),
-            ('ideal','MgO_NaCl')]),
-        target=10.978)
-qoi_db.add_qoi(
-        qoi_name='MgO_NaCl.fr_c',
-        qoi_type='E_formation',
-        structures=OrderedDict([
-            ('defect','MgO_NaCl_fr_c'),
-            ('ideal','MgO_NaCl')]),
-        target=8.986)
-qoi_db.add_qoi(
-        qoi_name='MgO_NaCl.sch',
-        qoi_type='E_formation',
-        structures=OrderedDict([
-            ('defect','MgO_NaCl_sch'),
-            ('ideal','MgO_NaCl')]),
-        target=5.067)
-qoi_db.add_qoi(
-        qoi_name='MgO_NaCl.001s',
-        qoi_type='E_surface',
-        structures=OrderedDict([
-            ('slab','MgO_NaCl_001s'),
-            ('ideal','MgO_NaCl')]),
-        target=0.05595)
 #------------------------------------------------------------------------------
 # QOI CONSTRAINTS
 # QOI constraints are performed in the order they are iterated through in
