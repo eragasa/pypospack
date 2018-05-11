8dc6d77245679afd12b7d17dc1caa0e0afcaddfd
Eugene J. Ragasa <eragasa@ufl.edu>
2018-05-11 05:07:19 -0400

    eragasa1

---
+++ examples/Ni__eam__born_bjs_fs/pareto_optimization_3.5NN/make_kde_file.py
@@ -0,0 +1,18 @@
+import os,copy,argparse
+from pypospack.pyposmat.data import PyposmatDataAnalyzer
+
+if __name__ == "__main__":
+    _fn_config=os.path.join(
+         "data__Ni__eam__born_exp_fs_02",
+         "pyposmat.config.in")
+    _fn_data=os.path.join(
+         "data__Ni__eam__born_exp_fs_02",
+         "pyposmat.results.0.mod.out")
+    _fn_kde_out=os.path.join(
+         "data__Ni__eam__born_exp_fs_02",
+         "pyposmat.kde.0.out")
+
+    pda = PyposmatDataAnalyzer(
+        fn_config=_fn_config,
+        fn_data=_fn_data)
+    pda.write_kde_file(filename=_fn_kde_out)
