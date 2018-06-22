76c941e98a52640ebd4a338c0695319af4370b9d
Eugene J. Ragasa <eragasa@ufl.edu>
2018-06-22 14:14:25 -0400

    added new logfile class with file locking

---
+++ pypospack/pyposmat/data/logfile.py
@@ -0,0 +1,32 @@
+import time
+import fcntl
+
+class PyposmatLogFile(object):
+
+    def __init__(self,filename=None):
+
+        self.filename = filename
+        self.time_sleep = 0.1
+        self.msg_format = "{}\n"
+
+    def write(self,s):
+
+        with open(self.filename,'a') as f:
+
+            # attempt to get lock
+            while True:
+                try:
+                    fcntl.flock(f,fcntl.LOCK_EX|fcntl.LOCK_NB)
+                    break
+                except io IOError as e:
+                    if e.errno != errno.EAGAIN:
+                        raise
+                    else:
+                        time.sleep(self.time_sleep)
+
+            # write message
+            f.write(self.msg_format.format(s))
+            
+            # release the lock
+            fcntl.flock(f, fcntl.LOCK_UN)
+
