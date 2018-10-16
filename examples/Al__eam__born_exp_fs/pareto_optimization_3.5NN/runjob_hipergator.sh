24f9ed8de7b212cf869207b735727258d3193583
seatonullberg <sullberg@ufl.edu>
2018-10-15 22:20:50 -0400

    aluminum example

--- examples/Al__eam__born_exp_fs/pareto_optimization_3.5NN/runjob_hipergator.sh
+++ examples/Al__eam__born_exp_fs/pareto_optimization_3.5NN/runjob_hipergator.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 #SBATCH --job-name=Ni_fs              # Job name
 #SBATCH --mail-type=NONE              # Mail events (NONE, BEGIN, END, FAIL)
-#SBATCH --ntasks=64                   # Number of MPI ranks
+#SBATCH --ntasks=8                   # Number of MPI ranks
 #SBATCH --cpus-per-task=1             # Number of cores per MPI rank 
 #SBATCH --distribution=cyclic:cyclic  # Distribute tasks cyclically on nodes 
 #SBATCH --mem-per-cpu=3000mb          # Memory per processor
-#SBATCH --time=24:00:00                # Time limit hrs:min:sec
+#SBATCH --time=12:00:00                # Time limit hrs:min:sec
 #SBATCH --output=job.out                   # Standard output and error log
 #SBATCH --error=job.err
-#SBATCH --qos=phillpot-b                # Queue you are submitting to 
+#SBATCH --qos=phillpot                # Queue you are submitting to 
 pwd; hostname; date
- 
-module load intel/2016.0.109 
-module load openmpi/1.10.2 
+
+module load intel/2018.1.163
+module load openmpi/3.0.0
 
 echo PYTHONPATH=$PYTHONPATH
+PYTHON_BIN=$(which python)
+echo PYTHON_BIN=$PYTHON_BIN
 echo python=$(which python)
 echo PATH=$PATH
 
 echo "start_time:$(date)"
-srun --mpi=pmi2 python mc_iterative_sampler.py
+srun --mpi=pmix_v1 $PYTHON_BIN mc_iterative_sampler.py
+# srun python mc_iterative_sampler.py --mpi=pmix_v1
+# mpiexec python mc_iterative_sampler.py
 echo "end_time:$(date)"
+
