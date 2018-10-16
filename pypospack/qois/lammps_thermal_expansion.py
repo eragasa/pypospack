32f662791d9f72785d2850ff9c76ca6464d46711
Eugene J. Ragasa <eragasa@ufl.edu>
2018-10-16 16:28:43 -0400

    cleanup of qois in pypospack

---
+++ pypospack/qois/lammps_thermal_expansion.py
@@ -0,0 +1,151 @@
+from collections import OrderedDict
+import numpy as np
+from pypospack.qoi import Qoi
+
+class ThermalExpansion(Qoi):
+
+    """
+
+    Args:
+        temperature_min (float,int): beginning of the temperature range in Kelvin
+        temperature_max (float,int): end of the temperature range in Kelvin
+        temperature_step (float,int): increments of the temperature range in Kelvin
+        time_total (int): total simulation time in fs
+        time_step (int): simulation time step in fs
+    """
+    def __init__(self,qoi_name,structures,
+            temperature_min=0,
+            temperature_max=2700,
+            temperature_step=100,
+            time_total=10,
+            time_step=0.001,
+            supercell=[5,5,5]):
+
+        _qoi_name = qoi_name
+        _qoi_type = 'lmps_thermal_expansion'
+
+        _structures = OrderedDict()
+        _structures['ideal'] = structures['ideal']
+
+        Qoi.__init__(self,
+                qoi_name=_qoi_name,
+                qoi_type=_qoi_type,
+                structures=_structures)
+    
+        self.temperature_min = temperature_min
+        self.temperature_max = temperature_max
+        self.temperature_step = temperature_step
+   
+        self.time_total=time_total
+        self.time_step=time_step
+
+        self.supercell = supercell
+    
+    def determine_tasks(self):
+
+        T = self.temperature_min
+        
+        while T <= self.temperature_max:
+            if T == 0:
+                _ideal_structure_name = self.structures['ideal']
+                _ideal_task_type = 'lmps_min_all'
+                _ideal_task_name = '{}.{}'.format(
+                        _ideal_structure_name,
+                        _ideal_task_type
+                        )
+                _bulk_structure_name = None
+
+                self.add_task(
+                        task_type=_ideal_task_type,
+                        task_name=_ideal_task_name,
+                        task_structure=_ideal_structure_name,
+                        bulk_structure_name=_bulk_structure_name,
+                        )
+            else:
+                _ideal_structure_name = self.structures['ideal']
+                _ideal_task_type = 'lmps_npt'.format(T)
+                _ideal_task_name = '{}.{}_{}'.format(
+                        _ideal_structure_name,
+                        _ideal_task_type,
+                        T
+                        )
+                _bulk_structure_name = None
+
+                self.add_task(
+                        task_type=_ideal_task_type,
+                        task_name=_ideal_task_name,
+                        task_structure=_ideal_structure_name,
+                        bulk_structure_name=_bulk_structure_name,
+                        task_options={
+                            'temperature':T,
+                            'time_total':self.time_total,
+                            'time_step':self.time_step,
+                            'supercell':self.supercell}
+                        )
+
+            T = T + self.temperature_step
+
+    def calculate_thermal_expansion_coefficient(self,temperatures,lattice_constants):
+
+        assert isinstance(temperatures,list)
+        assert isinstance(lattice_constants,list)
+        T = list(temperatures)
+        a0 = list(lattice_constants)
+
+        a0_at_0K = a0[0]
+        for i,v in enumerate(a0):
+            a0[i] = v/a0_at_0K-1
+
+        
+        T = np.array(temperatures)
+        a0 = np.array(a0)
+
+        print(T)
+        print(a0)
+
+        T = T[:,np.newaxis]            # T needs to be a column vector
+        # model is y = a*x
+        alpha_L,_,_,_ = np.linalg.lstsq(T,a0)
+        print('alpha_L:{}'.format(alpha_L))
+        return alpha_L
+    
+    def calculate_qois(self,task_results):
+        
+        _prefix = '{}.{}'.format(self.structures['ideal'],self.qoi_type)
+        s = self.structures['ideal']
+        T = self.temperature_min
+
+        lattice_constants = OrderedDict()
+        while T <= self.temperature_max:
+            if T == 0:
+                lattice_constants[T] = np.sqrt(
+                    task_results["{}.lmps_min_all.a11".format(s)]**2 \
+                    + task_results['{}.lmps_min_all.a12'.format(s)]**2 \
+                    + task_results["{}.lmps_min_all.a13".format(s)]**2
+                )
+            else:
+
+                try:
+                    lattice_constants[T] = task_results["{}.lmps_npt_{}.a1".format(s,T)]
+                except KeyError as e:
+                    for k,v in task_results.items():
+                        print(k,v)
+                    raise
+
+            T = T + self.temperature_step
+
+        self.qois = OrderedDict()
+        
+        # add lattice constants at different temperatures
+        for k,v in lattice_constants.items():
+            self.qois['{}.a0_{}'.format(_prefix,T)] = v
+       
+        _temperatures = [k for k,v in lattice_constants.items()]
+        _lattice_constants = [v for k,v in lattice_constants.items()]
+        
+        # add thermal expansion coefficient
+        self.qois['{}.thermal_expansion_coefficient'.format(_prefix)] = \
+            self.calculate_thermal_expansion_coefficient(
+                    temperatures=_temperatures,
+                    lattice_constants=_lattice_constants
+            )
---
+++ pypospack/qois/qoi_manager.py
@@ -0,0 +1,257 @@
+"""
+EJR 09OCT2018 - refactored from pypospack.qoi module, as part of the effort to move all qoi classes as a pypospack.qoi.package, 
+"""
+import copy,importlib
+from collections import OrderedDict
+from pypospack.qoi import QoiDatabase
+from pypospack.qoi import get_qoi_map
+
+class QoiManager(object):
+    """ Manager of Quantities of Interest 
+    
+    This class manages quantities of interest the simulation of multiple 
+    quantities of interest will often require the results from the same
+    simulation.  The purpose of this class is to identify the values required
+    from different simulations, and then identify which simulations need to
+    be done first.
+
+    Args:
+        qoi_info (str,QoiDatabase,None):  If set to str, then this arguement 
+            is treated as a filename used to configure a QoiDatabase object.  If
+            QoiDatabase is passed, the reference is set to the qoi_info attribute.
+            Default value is None, where the attribute is left as None.
+    Attributes:
+        qoi_info(pypospack.qoi.QoiDatabase)
+        qoi_names (list): list of qoi_names
+        qoi_targets (dict): key is the qoi_name, value is the reference value
+        qoi_errors (dict): key is the qoi_name, value is the error
+        qoi_dict (dict): 
+    """ 
+    def __init__(self,qoi_database= None,fullauto=True):
+        assert any([
+            isinstance(qoi_database,QoiDatabase),
+            type(qoi_database) is type(None),
+            type(qoi_database) is OrderedDict,
+            type(qoi_database) is str,
+            ])
+
+        #<--------- initialize attributes
+        self.qoidb = None
+        self.obj_Qoi = None
+        self.obj_QoiMap = None
+        self.tasks = None
+
+        self.__init_QoiDatabase(qoi_database=qoi_database)
+  
+        if fullauto:
+            self.configure()
+            self.determine_tasks()
+
+    @property
+    def qoi_names(self):
+        return self.qoidb.qoi_names
+
+    def configure(self):
+        self.configure__get_QoiMap()
+        self.configure__obj_Qoi()
+
+    def configure__get_QoiMap(self):
+        self.obj_QoiMap = get_qoi_map()
+
+    def get_qoi_name(self,qoi_simulation_type,structures):
+
+        if qoi_simulation_type in ['lmps_phase_order']:
+            s1 = structures['low']
+            s2 = structures['high']
+            qoiname = '{}__{}.{}'.format(s1,s2,qoi_simulation_type)
+
+        elif qoi_simulation_type in ['lmps_min_all',
+                                     'lmps_min_none',
+                                     'lmps_elastic',
+                                     'lmps_thermal_expansion',
+                                     'gulp_gamma_phonons']:
+            s = structures['ideal']
+            qoiname = '{}.{}'.format(s,qoi_simulation_type)
+
+        elif qoi_simulation_type in ['lmps_defect',
+                                     'lmps_stacking_fault']:
+            s = structures['defect']
+            qoiname = '{}.{}'.format(s,qoi_simulation_type)
+
+        elif qoi_simulation_type in ['lmps_surface_energy']:
+            s = structures['slab']
+            qoiname = '{}.{}'.format(s,qoi_simulation_type)
+
+        else:
+            msg_err = 'Unknown qoi_simulation_type: {}'.format(
+                    qoi_simulation_type)
+            raise ValueError(msg_err)
+        
+        return qoiname
+    
+    def configure__obj_Qoi(self):
+        self.obj_qoi = OrderedDict()
+        self.qois = OrderedDict()
+        for qoik,qoiv in self.qoidb.qois.items():
+            self.qois[qoik] = OrderedDict()
+            self.qois[qoik]['qoi_name'] = None
+            self.qois[qoik]['qoi_type'] = qoiv['qoi_type']
+            self.qois[qoik]['qoi_structures'] = qoiv['structures']
+            self.qois[qoik]['qoi_ref'] = qoiv['target']
+            try:
+                self.qois[qoik]['qoi_options'] = qoiv['qoi_options']
+            except KeyError as e:
+                pass
+
+            for qoimapk,qoimapv in self.obj_QoiMap.items():
+                if qoiv['qoi_type'] in qoimapv['qoi']:
+                    _structures = None
+                    _qoi_simulation_type = qoimapk
+                    _qoiname = self.get_qoi_name(
+                        qoi_simulation_type=_qoi_simulation_type,
+                        structures=qoiv['structures'])
+                    
+                    _qoitype = qoiv['qoi_type']
+                    _module = qoimapv['module']
+                    _class = qoimapv['class']
+                    _structures = qoiv['structures']
+                    
+                    try:
+                        _qoi_options = qoiv['qoi_options']
+                    except KeyError as e:
+                        _qoi_options = None
+                    
+                    self._add_obj_Qoi(
+                         qoi_name = _qoiname,
+                         module_name = _module,
+                         class_name = _class,
+                         structures = _structures,
+                         qoi_options = _qoi_options)
+                    self.qois[qoik]['qoi_name'] = '{}.{}'.format(_qoiname,_qoitype)
+
+    def __init_QoiDatabase(self,qoi_database=None):
+
+        if isinstance(qoi_database,QoiDatabase):
+            self.__init_QoiDatabase_from_QoiDatabase(
+                    obj_QoiDatabase=qoi_database)
+        elif isinstance(qoi_database,str):
+            self.__init_QoiDatabase_from_file(
+                    qoi_database_filename=qoi_database)
+        elif isinstance(qoi_database,OrderedDict):
+            self.__init_QoiDatabase_from_OrderedDict(
+                    qoi_database_OrderedDict=qoi_database)
+        elif qoi_database is None:
+            self.__init_QoiDatabase_from_None()
+        else:
+            msg_err = (
+                "qoi_database argument must be None,QoiDatabase, or str"
+                )
+            raise ValueError(msg_err)
+    
+    def __init_QoiDatabase_from_None(self):
+        self.qoidb = QoiDatabase()
+
+    def __init_QoiDatabase_from_file(self,qoi_database_filename):
+        assert isinstance(qoi_database_filename,str)
+        self.qoidb = QoiDatabase()
+        self.qoidb.read(filename=qoi_database_filename) 
+
+    def __init_QoiDatabase_from_QoiDatabase(self,obj_QoiDatabase):
+        assert isinstance(obj_QoiDatabase,QoiDatabase)
+        self.qoidb = copy.deepcopy(obj_QoiDatabase)
+
+    def __init_QoiDatabase_from_OrderedDict(self,qoi_database_OrderedDict):
+        assert isinstance(qoi_database_OrderedDict,OrderedDict)
+        self.qoidb = QoiDatabase(qoi_database_OrderedDict)
+
+    def _add_obj_Qoi(self,qoi_name,module_name,class_name,structures,qoi_options=None):
+        """
+
+        Args:
+            qoi_name(str):
+            module_name(str):
+            class_name(str):
+            structures(:obj:`list` of :obj:`str):
+
+        """
+        
+        if self.obj_Qoi is None: 
+            self.obj_Qoi = OrderedDict()
+
+        if qoi_name not in self.obj_Qoi.keys():
+            try:
+                module = importlib.import_module(module_name)
+                cls = getattr(module,class_name)
+
+                if qoi_options is None:
+                    self.obj_Qoi[qoi_name] = cls(
+                            qoi_name = qoi_name,
+                            structures = structures)
+                else:
+                    kwargs = qoi_options
+                    self.obj_Qoi[qoi_name] = cls(
+                            qoi_name = qoi_name,
+                            structures = structures,
+                            **kwargs)
+            except:
+                print('qoi_name(',type(qoi_name),'):',qoi_name)
+                print('module_name(',type(module_name),'):',module_name)
+                print('class_name(',type(class_name),'):',class_name)
+                print('structures(',type(structures),'):',structures)
+                raise
+
+    def determine_tasks(self):
+        self.tasks = OrderedDict()
+
+        for k_qoi, v_qoi in self.obj_Qoi.items():
+            v_qoi.determine_tasks()
+
+        for k_qoi,v_qoi in self.obj_Qoi.items():
+            for k_task, v_task in v_qoi.tasks.items():
+                self.add_task(
+                        task_name = k_task,
+                        task_dict = v_task)
+
+        return copy.deepcopy(self.tasks)
+
+    def add_task(self,task_name,task_dict):
+        assert isinstance(task_name,str)
+        assert isinstance(task_dict,dict)
+
+        if task_name not in self.tasks:
+            self.tasks[task_name] = copy.deepcopy(task_dict)
+
+    def calculate_qois(self,task_results):
+        """
+
+        Args:
+            task_results(OrderedDict):
+        """
+
+        # calculate the QOIs from the QOI objects
+        for n_qoi, o_qoi in self.obj_Qoi.items():
+            try:
+                o_qoi.calculate_qois(task_results=task_results)
+            except TypeError as e:
+                msg_err = "Cannot calculate qoi for \'{}\'.".format(n_qoi)
+                raise ValueError(msg_err+str(e))
+        
+        
+        for k_qoi,_ in self.qois.items():
+            try:
+                _qoi_id = self.qois[k_qoi]['qoi_name']
+                _obj_qoi_id = _qoi_id[:_qoi_id.rindex('.')]
+                _qoi_val = self.obj_Qoi[_obj_qoi_id].qois[_qoi_id]
+            except:
+                print('all_k_qois:')
+                for k,v in self.qois.items():
+                    print(k,v)
+                print('k_qoi:{}'.format(k_qoi))
+                print('v_qoi:{}'.format(self.qois[k_qoi]))
+                print('obj_qoi_id:{}'.format(_obj_qoi_id))
+                print('qoi_id:{}'.format(_qoi_id))
+                print(self.obj_Qoi)
+                raise
+            _qoi_ref = self.qois[k_qoi]['qoi_ref']
+            self.qois[k_qoi]['qoi_val'] = _qoi_val
+            self.qois[k_qoi]['qoi_err'] = _qoi_val-_qoi_ref
