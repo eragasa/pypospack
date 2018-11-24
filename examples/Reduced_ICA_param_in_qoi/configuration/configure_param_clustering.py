68108844b256ab18b4e9511d0d1fceee9560ffda
seatonullberg <seatonullberg@gmail.com>
2018-11-24 17:22:28 -0500

    more reduced examples

---
+++ examples/Reduced_ICA_param_in_qoi/configuration/configure_param_clustering.py
@@ -0,0 +1,61 @@
+from collections import OrderedDict
+from pypospack.pyposmat.data.pipeline import PyposmatPipeline
+
+pipeline_configuration = OrderedDict()
+
+# define first segment (normalization)
+pipeline_configuration[0] = OrderedDict() # int keys indicate step number
+pipeline_configuration[0]['segment_type'] = 'preprocess'
+pipeline_configuration[0]['function_calls'] = OrderedDict()
+pipeline_configuration[0]['function_calls'][0]= OrderedDict()  # int keys allow multiple calls to same function
+pipeline_configuration[0]['function_calls'][0]['function'] = 'normalize_standard_scaler'
+pipeline_configuration[0]['function_calls'][0]['args'] = OrderedDict()
+pipeline_configuration[0]['function_calls'][0]['args']['cols'] = ['param']
+pipeline_configuration[0]['function_calls'][0]['args']['kwargs'] = OrderedDict()
+pipeline_configuration[0]['function_calls'][0]['args']['kwargs']['standard_scaler'] = OrderedDict()
+pipeline_configuration[0]['function_calls'][0]['args']['kwargs']['standard_scaler']['copy'] = False
+pipeline_configuration[0]['function_calls'][0]['args']['kwargs']['standard_scaler']['with_mean'] = True
+pipeline_configuration[0]['function_calls'][0]['args']['kwargs']['standard_scaler']['with_std'] = True
+
+# define second segment (ICA transformation)
+pipeline_configuration[1] = OrderedDict()
+pipeline_configuration[1]['segment_type'] = 'pca'
+pipeline_configuration[1]['function_calls'] = OrderedDict()
+pipeline_configuration[1]['function_calls'][0]= OrderedDict()
+pipeline_configuration[1]['function_calls'][0]['function'] = 'transform_ica'
+pipeline_configuration[1]['function_calls'][0]['args'] = OrderedDict()
+pipeline_configuration[1]['function_calls'][0]['args']['cols'] = ['n_param']
+pipeline_configuration[1]['function_calls'][0]['args']['clusters'] = None
+pipeline_configuration[1]['function_calls'][0]['args']['kwargs'] = OrderedDict()
+pipeline_configuration[1]['function_calls'][0]['args']['kwargs']['ica'] = OrderedDict()
+
+# define third segment (clustering)
+pipeline_configuration[2] = OrderedDict()
+pipeline_configuration[2]['segment_type'] = 'cluster'
+pipeline_configuration[2]['function_calls'] = OrderedDict()
+pipeline_configuration[2]['function_calls'][0]= OrderedDict()
+pipeline_configuration[2]['function_calls'][0]['function'] = 'cluster_kmeans'
+pipeline_configuration[2]['function_calls'][0]['args'] = OrderedDict()
+# throw out ica 0
+pipeline_configuration[2]['function_calls'][0]['args']['abs_cols'] = ['ica_1', 'ica_2', 'ica_3', 'ica_4', 'ica_5', 'ica_6']
+pipeline_configuration[2]['function_calls'][0]['args']['kwargs'] = OrderedDict()
+pipeline_configuration[2]['function_calls'][0]['args']['kwargs']['kmeans'] = OrderedDict()
+pipeline_configuration[2]['function_calls'][0]['args']['kwargs']['kmeans']['n_clusters'] = 10
+
+# define fourth segment (plotting)
+pipeline_configuration[3] = OrderedDict()
+pipeline_configuration[3]['segment_type'] = 'plot'
+pipeline_configuration[3]['function_calls'] = OrderedDict()
+pipeline_configuration[3]['function_calls'][0]= OrderedDict()
+pipeline_configuration[3]['function_calls'][0]['function'] = 'plot_by_cluster'
+pipeline_configuration[3]['function_calls'][0]['args'] = OrderedDict()
+pipeline_configuration[3]['function_calls'][0]['args']['x_axis'] = 'ica_1'
+pipeline_configuration[3]['function_calls'][0]['args']['y_axis'] = 'ica_2'
+pipeline_configuration[3]['function_calls'][0]['args']['filename'] = 'param_clusters_in_ica_space.png'
+
+
+if __name__ == "__main__":
+    pipeline = PyposmatPipeline()
+    fn = __file__.replace('.py', '.in')
+    pipeline.write_configuration(filename=fn,
+                                 d=pipeline_configuration)
---
+++ examples/Reduced_Kernel_param_in_qoi/run_pipeline.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+import pypospack.utils
+from pypospack.pyposmat.data.pipeline import PyposmatPipeline
+
+pypospack_root_dir = pypospack.utils.get_pypospack_root_directory()
+configuration_dir = 'examples/Reduced_Kernel_param_in_qoi/configuration/'
+
+config_fn_0 = os.path.join(pypospack_root_dir,
+                           configuration_dir,
+                           'configure_param_clustering.in')
+config_fn_1 = os.path.join(pypospack_root_dir,
+                           configuration_dir,
+                           'configure_qoi_pca_transform.in')
+config_fn_2 = os.path.join(pypospack_root_dir,
+                           configuration_dir,
+                           'configure_qoi_plot.in')
+
+pyposmat_data_fn = os.path.join(pypospack_root_dir,
+                                'data/Ni__eam__born_exp_fs__3.5NN/pyposmat.kde.4.out')
+
+
+if __name__ == "__main__":
+    # normalize, pca transform, and cluster parameter space
+    pipeline_0 = PyposmatPipeline(configuration_fn=config_fn_0,
+                                data_fn=pyposmat_data_fn)
+    pipeline_0.read_configuration(config_fn_0)
+    pipeline_0.read_data(pyposmat_data_fn)
+    pipeline_0.run()
+
+    # normalize and pca transform qoi space
+    pipeline_1 = PyposmatPipeline(configuration_fn=config_fn_1,
+                                    data_fn=pyposmat_data_fn)
+    pipeline_1.read_configuration(config_fn_1)
+    pipeline_1.read_data(pyposmat_data_fn)
+    pipeline_1.run()
+
+    pipeline_1.df['cluster_id'] = pipeline_0.df['cluster_id']  # transfer cluster labels
+
+    # plot parameter clusters in qoi space
+    pipeline_2 = PyposmatPipeline(configuration_fn=config_fn_2,
+                                  df=pipeline_1.df)
+    pipeline_2.read_configuration(config_fn_2)
+    pipeline_2.run()
