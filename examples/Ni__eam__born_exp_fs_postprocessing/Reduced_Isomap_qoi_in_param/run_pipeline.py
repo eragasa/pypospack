e179c7ef8a24e6fc2a9547af2c14d4d431f21133
Seaton <seatonullberg@gmail.com>
2018-12-02 21:55:09 -0500

    reduced isomap plots

---
+++ examples/Ni__eam__born_exp_fs_postprocessing/Reduced_Isomap_qoi_in_param/run_pipeline.py
@@ -0,0 +1,43 @@
+import os
+import pypospack.utils
+from pypospack.pyposmat.data.pipeline import PyposmatPipeline
+
+pypospack_root_dir = pypospack.utils.get_pypospack_root_directory()
+configuration_dir = 'examples/Ni__eam__born_exp_fs_postprocessing/Reduced_Isomap_qoi_in_param/configuration/'
+
+config_fn_0 = os.path.join(pypospack_root_dir,
+                           configuration_dir,
+                           'configure_qoi_clustering.in')
+config_fn_1 = os.path.join(pypospack_root_dir,
+                           configuration_dir,
+                           'configure_param_isomap.in')
+config_fn_2 = os.path.join(pypospack_root_dir,
+                           configuration_dir,
+                           'configure_final_plot.in')
+
+pyposmat_data_fn = os.path.join(pypospack_root_dir,
+                                'data/Ni__eam__born_exp_fs__3.5NN/pyposmat.kde.20.out')
+
+
+if __name__ == "__main__":
+    # normalize, pca transform, and cluster qoi space
+    pipeline_0 = PyposmatPipeline(configuration_fn=config_fn_0,
+                                  data_fn=pyposmat_data_fn)
+    pipeline_0.read_configuration(config_fn_0)
+    pipeline_0.read_data(pyposmat_data_fn)
+    pipeline_0.run()
+
+    # normalize and pca transform param space
+    pipeline_1 = PyposmatPipeline(configuration_fn=config_fn_1,
+                                  data_fn=pyposmat_data_fn)
+    pipeline_1.read_configuration(config_fn_1)
+    pipeline_1.read_data(pyposmat_data_fn)
+    pipeline_1.run()
+
+    pipeline_1.df['cluster_id'] = pipeline_0.df['cluster_id']  # transfer cluster labels
+
+    # plot qoi clusters in parameter space
+    pipeline_2 = PyposmatPipeline(configuration_fn=config_fn_2,
+                                  df=pipeline_1.df)
+    pipeline_2.read_configuration(config_fn_2)
+    pipeline_2.run()
