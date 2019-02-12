7143ebae90c76ee53684fe3c8f2c0eab9b9a9841
Seaton <seatonullberg@gmail.com>
2019-02-12 00:35:40 -0500

    accurate percent error parallel plot

--- examples/Ni__eam__born_exp_fs__sensitivityanalysis/parallel_plot_script.py
+++ examples/Ni__eam__born_exp_fs__sensitivityanalysis/parallel_plot_script.py
@@ -11,52 +11,59 @@ import copy
 
 
 if __name__ == "__main__":
-    configuration_path = "/Users/seaton/python-repos/pypospack/examples/Ni__eam__born_exp_fs__sensitivityanalysis/data__from_pareto_optimization/pyposmat.config.in"
-    data_path = "/Users/seaton/python-repos/pypospack/examples/Ni__eam__born_exp_fs__sensitivityanalysis/data__from_pareto_optimization/pyposmat.kde.5.out"
-
+    # define paths to configuration and data files
+    configuration_path = "/home/seaton/python-repos/pypospack/examples/Ni__eam__born_exp_fs__sensitivityanalysis/data__from_pareto_optimization/pyposmat.config.in"
+    data_path = "/home/seaton/python-repos/pypospack/examples/Ni__eam__born_exp_fs__sensitivityanalysis/data__from_pareto_optimization/pyposmat.kde.5.out"
+    
+    # init the configuration object
     o_config = PyposmatConfigurationFile()
     o_config.read(configuration_path)
-
+    
+    # init the data file object
     o_data = PyposmatDataFile()
     o_data.read(data_path)
-
+    
+    # normalize the QOIs to prepare for tSNE
     o_normalizer = StandardScaler()
-    normal_parameter_arr = o_normalizer.fit_transform(o_data.parameter_df)
-    normal_parameter_df = pd.DataFrame(data=normal_parameter_arr, columns=o_data.parameter_names)
-
+    normal_qoi_arr = o_normalizer.fit_transform(o_data.qoi_df)
+    normal_qoi_df = pd.DataFrame(data=normal_qoi_arr, columns=o_data.qoi_names)
+    
+    # learn and apply tSNE manifold to the normal QOIs
     o_tsne = TSNE()
-    tsne_arr = o_tsne.fit_transform(normal_parameter_df)
-
-    # o_cluster = DBSCAN()
-    # cluster_ids = o_cluster.fit_predict(tsne_cols)
-
-    o_cluster = KMeans(n_clusters=5)
+    tsne_arr = o_tsne.fit_transform(normal_qoi_df)
+    
+    # find KMeans clusters in the tSNE space
+    o_cluster = KMeans(n_clusters=3)
     cluster_ids = o_cluster.fit_predict(tsne_arr)
-
-    normal_parameter_df['tsne_0'] = tsne_arr[:,0]
-    normal_parameter_df['tsne_1'] = tsne_arr[:,1]
-    normal_parameter_df['cluster_id'] = cluster_ids
+    
+    # plot the tSNE space by cluster
+    normal_qoi_df['tsne_0'] = tsne_arr[:,0]
+    normal_qoi_df['tsne_1'] = tsne_arr[:,1]
+    normal_qoi_df['cluster_id'] = cluster_ids
     for cid in set(cluster_ids):
-        subset = normal_parameter_df.loc[normal_parameter_df['cluster_id'] == cid]
+        subset = normal_qoi_df.loc[normal_qoi_df['cluster_id'] == cid]
         _x = subset['tsne_0']
         _y = subset['tsne_1']
         plt.scatter(x=_x, y=_y, s=1, cmap=plt.get_cmap("Set1"))
-    plt.savefig("parameter_clusters_in_tsne_space.png")
+    plt.savefig("qoi_clusters_in_tsne_space.png")
     plt.show()
 
-
-    percent_error_df = copy.deepcopy(o_data.error_df)
-    for e_name in o_data.error_names:
-        percent_error_df[e_name] = o_data.error_df[e_name].div(o_data.error_df[e_name].mean())
+    # generate a DataFrame containing percent errors
+    percent_error_df = copy.deepcopy(o_data.qoi_df)
+    for q_name in o_data.qoi_names:
+        percent_error_df[q_name] = (o_data.qoi_df[q_name]-o_config.qoi_targets[q_name])/o_config.qoi_targets[q_name]
     percent_error_df['cluster_id'] = cluster_ids
 
-
-    centroid_df = percent_error_df.groupby(['cluster_id']).median()
-    centroid_df['cluster_id'] = range(5)
+    print("min: ", o_data.qoi_df["Ni_fcc.isf"].min())
+    print("max: ", o_data.qoi_df["Ni_fcc.isf"].max())
+    print("mean: ", o_data.qoi_df["Ni_fcc.isf"].mean())
+    print("target: ", o_config.qoi_targets["Ni_fcc.isf"])
+    # centroid_df = percent_error_df.groupby(['cluster_id']).median()
+    # centroid_df['cluster_id'] = range(5)
 
     plt.xticks(rotation=80)
-    ax = parallel_coordinates(centroid_df, 'cluster_id', colormap=plt.get_cmap("Set1"))
-    plt.title("Normalized Errors Clustered in Parameter Space by tSNE+KMeans")
+    ax = parallel_coordinates(percent_error_df, 'cluster_id', colormap=plt.get_cmap("Set1"))
+    plt.title("Percent Error Across QOI Clusters (tSNE+KMeans)")
     plt.tight_layout()
-    plt.savefig("parallel_plot_norm_err_by_cluster.png")
+    plt.savefig("parallel_plot_pct_err_by_qoi_cluster.png")
     plt.show()
