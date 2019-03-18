bec56f1dfc1e3a140a2b32154fd643e08d49f123
seatonullberg <sullberg@ufl.edu>
2019-03-18 12:52:27 -0400

    documentation

--- pypospack/pyposmat/data/referencedatafile.py
+++ pypospack/pyposmat/data/referencedatafile.py
@@ -2,6 +2,11 @@ import json
 
 
 class PyposmatReferenceDataFile(object):
+    """ Used to interface with data from publications for validation purposes.
+    
+    Args:
+        filename(str, optional): the filename of the data file to read (must be JSON).
+    """
 
     def __init__(self, filename=None):
         self.filename = filename
@@ -23,17 +28,18 @@ class PyposmatReferenceDataFile(object):
             return self._qois
 
     def read(self, filename=None):
+        # filename uses self.filename if not set
         if filename is None:
             if self.filename is None:
                 raise ValueError("must provide a filename to read")
             else:
                 filename = self.filename
-
+        # filename must refer to a JSON file
         if not filename.endswith(".json"):
             raise ValueError("filename must be a json file")
-        
+        # read JSON into a dict
         with open(filename) as f:
             raw_json = json.load(f)
-
+        # set the private attributes which the properties pull from
         self._bibtex = raw_json.pop("bibtex", None)
         self._qois =  raw_json
