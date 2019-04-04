98189293e669b6921833633c7d77f4108f80c16d
Eugene J. Ragasa <eragasa@ufl.edu>
2019-04-04 02:36:36 -0400

    reference potentials for Si stillinger weber

---
+++ examples/Si__sw/reference_potentials/Si__sw__reference_potentials.py
@@ -0,0 +1,10 @@
+from collections import OrderedDict
+
+reference_potentials = OrderedDict()
+reference_potentials['SW'] = OrderedDict([
+])
+reference_potentials['VBWM'] = OrderedDict([
+])
+reference_potentials['PG'] = OrderedDict([
+])
+
