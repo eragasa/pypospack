872d96cd0314641d466aa781c0bcb7850e58882c
Eugene J. Ragasa <eragasa@ufl.edu>
2019-05-30 12:40:21 -0400

    updating examples dir

---
+++ examples/Si__sw/pareto_optimization_npt/cleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -rf MgO_NaCl.lmps_elastic
+rm -rf MgO_NaCl_001s.lmps_min_pos
+rm -rf MgO_NaCl_fr_c.lmps_min_pos
+rm -rf MgO_NaCl.lmps_min_all
+rm -rf MgO_NaCl_fr_a.lmps_min_pos
+rm -rf MgO_NaCl_sch.lmps_min_pos
