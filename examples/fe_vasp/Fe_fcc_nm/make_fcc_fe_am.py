ba546b99fcf597ef660c4c4274f858d6b8cd4693
Eugene Ragasa <eragasa@users.noreply.github.com>
2019-09-03 02:07:00 -0400

    structures

---
+++ examples/fe_vasp/Fe_fcc_nm/make_fcc_fe_am.py
@@ -0,0 +1,17 @@
+import os, shutil, subprocess
+from pypospack.crystal import SimulationCell
+import pypospack.io.vasp as vasp
+import pypospack.crystal as crystal
+import pypospack.io.slurm as minimize_init_slurm
+
+# define simulation cell
+cell = SimulationCell()
+cell.a0 = 3.447
+cell.add_atom('Fe', [0.0, 0.0, 0.0], magmom=2.22)
+cell.add_atom('Fe', [0.5, 0.5, 0.0], magmom=2.22)
+cell.add_atom('Fe', [0.5, 0.0, 0.5], magmom=2.22)
+cell.add_atom('Fe', [0.0, 0.5, 0.5], magmom=2.22)
+
+poscar = vasp.Poscar(cell)
+poscar.write("POSCAR")
+print(poscar.get_magmom_tag())
